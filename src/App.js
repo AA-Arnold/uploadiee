d2149def093fe2d3c86508b41a64e65789387f0d
AA_Arnold <amasoharnold@gmail.com>
2021-07-23 13:21:15 -1100

    completed the implementatio with  cloudinary to upload pictures

--- src/App.js
+++ src/App.js
@@ -1,47 +1,63 @@
 import React from "react";
-import { Transformation, Image } from 'cloudinary-react';
+// import { Transformation, Image } from 'cloudinary-react';
 import './App.css';
 
 function App() {
+  const [loading, setLoading] = React.useState(false);
   const [image, setImage] = React.useState("");
-  const uploadImage = () => {
+
+  const uploadImage = async e => {
+    const files = e.target.files[0]
     const data = new FormData()
-    data.append("file", image)
+    data.append("file", files)
     data.append("upload_preset", "interview")
-    data.append("cloud_name", "aaatechie")
-    fetch("https://api.cloudinary.com/v1_1/aaatechie/image/upload", {
+    setLoading(true)
+    const res = await fetch("https://api.cloudinary.com/v1_1/aaatechie/image/upload", {
       method: "post",
       body: data
     })
       .catch(err => console.log(err))
 
-    console.log(data)
+    const file = await res.json()
+
+    console.log(file)
+
+    setImage(file.secure_url)
+    setLoading(false)
   }
   console.log(image)
+
+  const deleteImage = () => {
+    setImage("")
+  }
+
   return (
     <div className="App">
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>Uploads</li>
-        </ul>
-      </nav>
+      <header>
+        <div class="header-title-bar">
+          <h1>Upload Your Files</h1>
+        </div>
+      </header>
 
       <div className="App-body">
-        <div>
-          <input type="file" onChange={(e) => setImage(e.target.files[0])}></input>
-          <button onClick={uploadImage}>Upload</button>
-        </div>
 
-        <div>
-          <div>Preview</div>
-          <Image
+        <div className="image-upload">
+          <label htmlFor='file-input'>
+            <img src="https://www.freeiconspng.com/uploads/upload-icon-3.png" alt="Icon Drawing Upload" />
+          </label>
+          <input id='file-input' type="file" onChange={uploadImage}></input>
+
+          <h2>Uploads</h2>
+          {
+            loading ? <div>Loading...</div> : <img alt='' width='200px' height='200px' src={image} />
+          }
+          {/* <Image
             cloudName="aaatechie"
             publicId="https://res.cloudinary.com/aaatechie/image/upload/v1627068379/c7tqoc3lrae7cr1qtfvu.jpg"
             width="300">
             <Transformation width="200" crop="scale" angle="45" />
-          </Image>
-          <button>Delete</button>
+          </Image> */}
+          <button onClick={deleteImage}>Delete</button>
         </div>
       </div>
 
